75489df63558f6a4a91711eb485b83882a912f1d
d.phan@ucl.ac.uk <d.phan@ucl.ac.uk>
2021-06-23 00:13:05 -0500

    Last push from old Arch System.

---
+++ keyboards/preonic/keymaps/dphan90/config.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "../../rev3/config.h"
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(PREONIC_SOUND)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                  SONG(COLEMAK_SOUND), \
+                                  SONG(DVORAK_SOUND) \
+                                }
+#endif
+
+#define MUSIC_MASK (keycode != KC_NO)
+
+/*
+ * MIDI options
+ */
+
+/* enable basic MIDI features:
+   - MIDI notes can be sent when in Music mode is on
+*/
+
+#define MIDI_BASIC
+
+/* enable advanced MIDI features:
+   - MIDI notes can be added to the keymap
+   - Octave shift and transpose
+   - Virtual sustain, portamento, and modulation wheel
+   - etc.
+*/
+//#define MIDI_ADVANCED
+
+/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
+//#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+
+#define RGB_DI_PIN A1
+#define RGBLED_NUM 9
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_KNIGHT
+
--- keyboards/sofle/keymaps/dphan90/keymap.c
+++ keyboards/sofle/keymaps/dphan90/keymap.c
@@ -46,10 +46,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,     KC_3,       KC_4,      KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,     KC_E,       KC_R,      KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_MINS,
-  KC_ESC,   KC_A,   KC_S,     KC_D,       KC_F,      KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,     KC_C,       KC_V,      KC_B, KC_MUTE,    XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
+  KC_GRV,   KC_1,   KC_2,     KC_3,       KC_4,      KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  ALT_T(KC_TAB),   KC_Q,   KC_W,     KC_E,       KC_R,      KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_QUOT,
+  GUI_T(KC_ESC),   KC_A,   KC_S,     KC_D,       KC_F,      KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_BSPC,
+  KC_BSLS,  KC_Z,   KC_X,     KC_C,       KC_V,      KC_B,  _______,   _______,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
                  KC_LALT,  KC_LGUI,    KC_LOWER,   KC_LSFT, KC_SPC,           KC_ENT, KC_RCTRL, KC_RAISE, KC_LBRC, KC_RBRC
 ),
 
@@ -71,10 +71,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_COLEMAK] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_MINS,
-  KC_ESC,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_MUTE,      RGB_TOG,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  ALT_T(KC_TAB),   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_QUOT,
+  GUI_T(KC_ESC),   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_BSPC,
+  KC_BSLS,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, _______,     _______,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
                  KC_LALT,  KC_LGUI,   KC_LOWER,   KC_LSFT, KC_SPC,           KC_ENT, KC_RCTRL, KC_RAISE, KC_LBRC, KC_RBRC
 ),
 
@@ -93,10 +93,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_LOWER] = LAYOUT(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,                              KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, KC_F12,
-  _______,    _______,    _______,    _______,    _______,    _______,            KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______,  KC_BSLS,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   _______, _______,   _______, _______,  _______, _______,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
-  _______,  _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                               KC_6,    KC_7,    KC_8,   KC_9,    KC_0,   KC_DEL,
+  _______,  _______, _______, _______, _______, _______, KC_MUTE,       _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, _______,
                        _______, _______, _______, _______, _______,       _______, _______, KC_ADJUST, _______, _______
 ),
 
@@ -115,10 +115,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, KC_F12,
-  _______,    _______,    _______,    _______,    _______,    _______,            KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______,  KC_BSLS,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   _______, _______,   _______, _______,  _______, _______,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
-  _______,  _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______, _______,
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,                      KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,KC_LPRN, KC_RPRN, KC_DEL,
+  _______,  _______, _______, _______, _______, _______, KC_MUTE,       _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, _______,
                        _______, _______, _______, _______, _______,       _______, _______, KC_ADJUST, _______, _______
 ),
 
@@ -765,17 +765,47 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef ENCODER_ENABLE
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
+    if (IS_LAYER_ON(_RAISE)) {
+        if (index == 0) {
+            if (clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
+        } else if (index == 1) {
+            if (clockwise) {
+                tap_code(KC_PGUP);
+            } else {
+                tap_code(KC_PGDOWN);
+            }
         }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDOWN);
-        } else {
-            tap_code(KC_PGUP);
+    } else if (IS_LAYER_ON(_LOWER)) {
+        if (index == 0) {
+            if (clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
+        } else if (index == 1) {
+            if (clockwise) {
+                tap_code(KC_PGUP);
+            } else {
+                tap_code(KC_PGDOWN);
+            }
+        }
+    } else {
+        if (index == 0) {
+            if (!clockwise) {
+                tap_code(KC_LEFT);
+            } else {
+                tap_code(KC_RIGHT);
+            }
+        } else if (index == 1) {
+            if (clockwise) {
+                tap_code(KC_UP);
+            } else {
+                tap_code(KC_DOWN);
+            }
         }
     }
 }
