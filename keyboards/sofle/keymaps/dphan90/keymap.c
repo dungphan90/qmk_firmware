cd4024e18c0afcd31e354327fafecf5912251d14
d.phan@ucl.ac.uk <d.phan@ucl.ac.uk>
2021-07-14 21:06:20 -0500

    Remove the MT keys.

--- keyboards/sofle/keymaps/dphan90/keymap.c
+++ keyboards/sofle/keymaps/dphan90/keymap.c
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,     KC_3,       KC_4,      KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
-  ALT_T(KC_TAB),   KC_Q,   KC_W,     KC_E,       KC_R,      KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_QUOT,
-  GUI_T(KC_ESC),   KC_A,   KC_S,     KC_D,       KC_F,      KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,     KC_E,       KC_R,      KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_QUOT,
+  KC_ESC,   KC_A,   KC_S,     KC_D,       KC_F,      KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_BSPC,
   KC_BSLS,  KC_Z,   KC_X,     KC_C,       KC_V,      KC_B,  _______,   _______,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
                  KC_LALT,  KC_LGUI,    KC_LOWER,   KC_LSFT, KC_SPC,           KC_ENT, KC_RCTRL, KC_RAISE, KC_LBRC, KC_RBRC
 ),
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_COLEMAK] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
-  LCTL_T(KC_TAB),   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_QUOT,
-  GUI_T(KC_ESC),   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_BSPC,
-  LSFT_T(KC_BSLS),  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, _______,     _______,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_QUOT,
+  KC_ESC,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_BSPC,
+  KC_BSLS,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, _______,     _______,KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,
                  KC_LALT,  KC_LGUI,   KC_LOWER,   KC_LSFT, KC_SPC,           KC_ENT, KC_RCTRL, KC_RAISE, KC_LBRC, KC_RBRC
 ),
 
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______,   _______, _______,  _______, _______,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,                      KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,KC_LPRN, KC_RPRN, KC_DEL,
   _______,  _______, _______, _______, _______, _______, KC_MUTE,       _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, _______,
-                       _______, _______, _______, _______, _______,       _______, _______, KC_ADJUST, _______, _______
+                       _______, _______, KC_ADJUST, _______, _______,       _______, _______, _______, _______, _______
 ),
 
 /* ADJUST
