33bd9a5ab933428cd8c300a0f907f351a49ac06b
d.phan@ucl.ac.uk <d.phan@ucl.ac.uk>
2021-07-14 22:28:24 -0500

    Adding arrow keys to the left hand.

--- keyboards/sofle/keymaps/dphan90/keymap.c
+++ keyboards/sofle/keymaps/dphan90/keymap.c
@@ -94,9 +94,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
-  _______, _______,   _______, _______,  _______, _______,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
+  _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
   _______, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                               KC_6,    KC_7,    KC_8,   KC_9,    KC_0,   KC_DEL,
-  _______,  _______, _______, _______, _______, _______, KC_MUTE,       _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, _______,
+  _______,  KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, KC_MUTE,       _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, _______,
                        _______, _______, _______, _______, _______,       _______, _______, KC_ADJUST, _______, _______
 ),
 
@@ -116,9 +116,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
-  _______, _______,   _______, _______,  _______, _______,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
+  _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,                      KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,KC_LPRN, KC_RPRN, KC_DEL,
-  _______,  _______, _______, _______, _______, _______, KC_MUTE,       _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, _______,
+  _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, KC_MUTE,       _______, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END, _______, _______,
                        _______, _______, KC_ADJUST, _______, _______,       _______, _______, _______, _______, _______
 ),
 
